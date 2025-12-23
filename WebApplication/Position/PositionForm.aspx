3da311e9960334d3c9a0cbe4b8dba21d22009e65
Aditya Pratama Febriono <aditya.febriono@elnusa.co.id>
2025-12-23 10:07:25 +0700

    Add Module Position

---
+++ WebApplication/Position/PositionForm.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PositionForm.aspx.cs" Inherits="WebApplication.Position.PositionForm" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title>Position Form</title>
+</head>
+<body>
+<form id="form1" runat="server">
+
+    <h2><asp:Label ID="lblTitle" runat="server" /></h2>
+
+    Name:<br />
+    <asp:TextBox ID="txtName" runat="server" />
+    <asp:RequiredFieldValidator runat="server"
+        ControlToValidate="txtName"
+        ErrorMessage="Name wajib diisi"
+        ForeColor="Red" />
+    <br /><br />
+
+    Level:<br />
+    <asp:TextBox ID="txtLevel" runat="server" />
+    <br /><br />
+
+    <asp:Button ID="btnSave" runat="server" Text="Simpan" OnClick="btnSave_Click" />
+    <asp:Button ID="btnBack" runat="server" Text="Kembali" OnClick="btnBack_Click" />
+
+</form>
+</body>
+</html>
---
+++ WebApplication/Position/PositionList.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using PosModel = WebApplication.Models.Position;
+
+namespace WebApplication.Position
+{
+    public partial class PositionList : Page
+    {
+        private const string SessionKey = "Positions";
+
+        // Ambil data Position dari Session (sekali simpan, dipakai terus)
+        private List<PosModel> Positions
+        {
+            get
+            {
+                if (Session[SessionKey] == null)
+                {
+                    // data awal (dummy)
+                    Session[SessionKey] = new List<PosModel>
+                    {
+                        new PosModel { Id = 1, Name = "Staff", Level = "Junior" },
+                        new PosModel { Id = 2, Name = "Supervisor", Level = "Senior" }
+                    };
+                }
+                return (List<PosModel>)Session[SessionKey];
+            }
+            set { Session[SessionKey] = value; }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) BindGrid();
+        }
+
+        private void BindGrid()
+        {
+            gvPositions.DataSource = Positions.OrderBy(x => x.Id).ToList();
+            gvPositions.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            // aman pakai path absolut
+            Response.Redirect("PositionForm.aspx");
+        }
+
+        protected void gvPositions_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            int rowIndex = Convert.ToInt32(e.CommandArgument);
+            int id = Convert.ToInt32(gvPositions.DataKeys[rowIndex].Value);
+
+            if (e.CommandName == "EditRow")
+            {
+                Response.Redirect("PositionForm.aspx?id=" + id);
+                return;
+            }
+
+            if (e.CommandName == "DeleteRow")
+            {
+                var list = Positions;
+                var pos = list.FirstOrDefault(x => x.Id == id);
+                if (pos != null)
+                {
+                    list.Remove(pos);
+                    Positions = list;
+                }
+                BindGrid();
+            }
+        }
+    }
+}
